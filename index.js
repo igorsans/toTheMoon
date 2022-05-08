a9be8b73b4eeb810f87230a53a554c6bd06a3477
igorsans <igor.santos0@hotmail.com>
2022-05-08 15:43:36 -0300

    agora rodamos video
    video está funcionando

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 $("#btn").click(function (e) {
     e.preventDefault();
-    $('#date,#text,#img,#title').remove()
+    $('#date,#text,#media,#title').remove()
     var data = $('#data').val()
     let key = 'jPARZgxn9D6y8F2Cd2SQ4DEwm35UQRnqV8grHAI8'
     $.ajax({
         url: `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${data}`,
         success: function (resposta) {
             useData(resposta)
+            console.log(resposta);
         },
         error: function (erro) {
             console.log(erro);
@@ -15,12 +16,20 @@ $("#btn").click(function (e) {
 
     });
 
-    $(`<div id='date'></div> <div id='text'></div> <div id='title'></div> <div id='img'></div>`).appendTo("#results")
+    $(`<div id='date'></div> <div id='text'></div> <div id='title'></div> <div id='media'></div>`).appendTo("#results")
 });
 
 function useData(resposta) {    
     $('#date').prepend(`<h2>${resposta.date}</h2>`)
     $('#text').prepend(`<p>${resposta.explanation}</p>`)
     $('#title').prepend(`<h3>${resposta.title}</h3>`)
-    $('#img').prepend(`<img src='${resposta.url}'/>`)
+    if(resposta.media_type == "video"){
+        console.log('video');
+        $('#media').prepend(`<iframe width="420" height="315"
+        src='${resposta.url}'>
+        </iframe>`)
+    } else if (resposta.media_type == 'image'){
+        console.log('imagem');
+        $('#media').prepend(`<img src='${resposta.url}'/>`)
+    }
 }
